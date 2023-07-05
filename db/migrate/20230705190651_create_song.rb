8f920f1804bad10a2f524bd7c803d19ba5758c67
Zanna Fitch <zannafitch2004@gmail.com>
2023-07-05 14:26:22 -0500

    add the song index

---
+++ db/migrate/20230705190651_create_song.rb
@@ -0,0 +1,11 @@
+class CreateSong < ActiveRecord::Migration[7.0]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.integer :length
+      t.integer :play_count
+
+      t.timestamps
+    end
+  end
+end
